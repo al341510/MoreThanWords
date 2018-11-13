5e3258239a98d9e2a0b671e62e0a024750794a86
alexcercos <44820718+alexcercos@users.noreply.github.com>
2018-11-14 00:10:59 +0100

    Key-door system
    Destruccion de llaves, se abre la puerta al cogerla y se cierra al contacto con el jugador
    
    +correcciones

---
+++ 2D stuff/Assets/Scripts/PickUpKey.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpKey : MonoBehaviour {
+	private GameObject door;
+	private Animator anim;
+	// Use this for initialization
+	void Start () { //Funciona con una sola puerta en el nivel
+		door = GameObject.Find("Door");
+		anim = door.GetComponent<Animator>();
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		//si hay que coger mas llaves habria que poner otro script en player y cambiar alguna variable antes de destruirla
+		if (other.gameObject.CompareTag ("Player")) {
+			Destroy(gameObject);
+			anim.SetBool("openDoor", true);
+		}
+			
+	}
+}
