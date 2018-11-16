f98aabf3a6cc30fcf6bca8db40ddc8aa8b62d831
al364891 <al364891@ui.es>
2018-11-16 11:42:41 +0100

    declarar stats en personajes

---
+++ 2D stuff/Assets/Scripts/giantStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class giantStats : MonoBehaviour {
+
+    [SerializeField] private int life = 6;
+    [SerializeField] private int attack = 6;
+    [SerializeField] private bool fire = false;      //si tenemos poder de fuego
+    [SerializeField] private bool ice = false;       //si tenemos poder de hielo
+    [SerializeField] private bool damage = false;    //si recibe daño
+
+    /*// Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}*/
+}
---
+++ 2D stuff/Assets/Scripts/playerStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerStats : MonoBehaviour {
+
+    [SerializeField] private int life = 3;
+    [SerializeField] private int attack = 3;
+    [SerializeField] private int keys = 0;           //int en vez de bool, porque hablabamos de poner varias llaves
+    [SerializeField] private bool fire = false;      //si tenemos poder de fuego
+    [SerializeField] private bool ice = false;       //si tenemos poder de hielo
+    [SerializeField] private bool damage = false;    //si recibimos daño
+
+    /*// Use this for initialization
+	void Start () {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}*/
+
+}
