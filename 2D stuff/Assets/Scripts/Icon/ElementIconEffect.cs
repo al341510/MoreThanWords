7dd7a6e729710fab0297c2055ae9fa4d5995dfad
Nam <leh@uni-bremen.de>
2018-11-27 23:06:04 +0100

    Icon animation and Element on HUB finish

--- 2D stuff/Assets/Scripts/Icon/ElementIconEffect.cs
+++ 2D stuff/Assets/Scripts/Icon/ElementIconEffect.cs
@@ -21,6 +21,9 @@ public class ElementIconEffect : MonoBehaviour {
     private float timeE;
     private bool timeFlag = true;
 
+    private bool newMagic1 = false;
+    private bool newMagic2 = false;
+
     // Use this for initialization
     void Start () {
         magicTime = GetComponent<Image>();
@@ -30,10 +33,18 @@ public class ElementIconEffect : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        /*
+         add in player.class trigger when player actived magic, when a magic is activ
+         */
 
-        if (player.activeMagic.ToString() == "FIRE")
+        if (player.activeMagic.ToString() == "FIRE" )
         {
-
+            newMagic1 = true;
+            if (newMagic2 != false)
+            {
+                timeE = time;
+                newMagic2 = false;
+            }
             this.GetComponent<Image>().enabled = true;
             this.GetComponent<Image>().sprite = myFirstImage;
 
@@ -43,12 +54,22 @@ public class ElementIconEffect : MonoBehaviour {
             }
             else if (timeE < 1)
             {
+
+                magicTime.fillAmount = 1;
                 this.GetComponent<Image>().enabled = false;
                 player.activeMagic = Player.playerMagic.NEUTRAL;
+                timeE = time;
+                newMagic1 = false;
             }
         }
         else if (player.activeMagic.ToString() == "ICE")
         {
+            newMagic2 = true;
+
+            if (newMagic1 != false) {
+                timeE = time;
+                newMagic1 = false;
+            }
             this.GetComponent<Image>().enabled = true;
             this.GetComponent<Image>().sprite = mySecondImage;
 
@@ -58,8 +79,11 @@ public class ElementIconEffect : MonoBehaviour {
             }
             else if (timeE < 1)
             {
+                magicTime.fillAmount = 1;
                 this.GetComponent<Image>().enabled = false;
                 player.activeMagic = Player.playerMagic.NEUTRAL;
+                timeE = time;
+                newMagic2 = false;
             }
         }
         
