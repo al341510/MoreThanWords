97c512033b6d72f4c0195a6aa030254dbb95311b
Ruben <al341510@uji.es>
2018-11-28 02:23:21 +0100

    Añadido un angulo provisional a las flechas (mas detallado que el fijo anterior)

--- 2D stuff/Assets/Scripts/NPCArcherAttack.cs
+++ 2D stuff/Assets/Scripts/NPCArcherAttack.cs
@@ -9,22 +9,26 @@ public class NPCArcherAttack : Attacker {
     public override void Attack(float x, float y, float direction)
     {
         this.x = x; // X del arquero
-        this.y = y; // Y del arquero
+        this.y = y; // Y del arquero - HAY UN DESAJUSTE DEL CENTRO DEL ARQUERO A LA POSICION Y DEL ARQUERO - APROXIMACION DE 1 USADA EN yDiff
         this.direction = direction; // es 1 si mira a la izquierda // es casi 0 (numero muy pequeño) si mira a la derecha
         Invoke("SpawnArrow", 0.85f); // Añade delay al metodo
     }
 
     private void SpawnArrow()
     {
+        float angulo;
+        float xDiff = GameObject.FindGameObjectWithTag("Player").transform.position.x - this.x;
+        float yDiff = GameObject.FindGameObjectWithTag("Player").transform.position.y - this.y - 1;
+        angulo = Mathf.Atan2(yDiff, xDiff) * 180 / Mathf.PI;
         GameObject newArrow = Instantiate(arrow, new Vector3(x, y + 0.5f, 0), Quaternion.Euler(0, direction, 0)) as GameObject;
         //Debug.Log(direction);
         if (direction < 0.01)
         {
-            newArrow.GetComponent<ArrowForce>().SetAngle(20);
+            newArrow.GetComponent<ArrowForce>().SetAngle(angulo + 25);
         }
         else
         {
-            newArrow.GetComponent<ArrowForce>().SetAngle(180 - 20);
+            newArrow.GetComponent<ArrowForce>().SetAngle(angulo - 25);
         }
     }
 }
